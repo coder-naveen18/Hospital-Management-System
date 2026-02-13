b90b772f622ec97808529d34bf8e15dcd3730455
coder-naveen18 <codersahu18@gmail.com>
2026-02-13 12:39:43 +0530

    Enhance Patient model and serializers with additional fields and validation

--- Backend/Hospital/core/models.py
+++ Backend/Hospital/core/models.py
@@ -1,6 +1,9 @@
+import uuid
+from datetime import date
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Custom User model extending AbstractUser
 class User(AbstractUser):
     class Roles(models.TextChoices):
@@ -33,26 +36,69 @@ class Doctor(models.Model):
     
 
 class Patient(models.Model):
-    patient_id = models.CharField(max_length=20, unique=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    class Gender(models.TextChoices):
+        MALE = 'MALE', 'Male'
+        FEMALE = 'FEMALE', 'Female'
+        OTHER = 'OTHER', 'Other'    
+
+    patient_id = models.CharField(max_length=20, unique=True, editable=False)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     dob = models.DateField()
-    gender = models.CharField(max_length=10)
-    phone = models.CharField(max_length=15)
+    gender = models.CharField(max_length=10, choices=Gender.choices)
+    phone = models.CharField(
+        max_length=15,
+        validators=[RegexValidator(r'^\d{10}$', "Phone number must be 10 digits")]
+    )
     email = models.EmailField(blank=True, null=True)
 
     address = models.TextField()
     emergency_contact = models.CharField(max_length=15)
-    insurance_details = models.TextField(blank=True)
+
+    insurance_provider = models.CharField(max_length=100, blank=True, null=True)
+    policy_number = models.CharField(max_length=100, blank=True, null=True)
+
+    photo = models.ImageField(upload_to="patients/", blank=True, null=True)
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="registered_patients"
+    )
+
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="updated_patients"
+    )
+
+    @property
+    def age(self):
+        today = date.today()
+        age = today.year - self.dob.year
+        if today < self.dob.replace(year=today.year):
+            age -= 1
+        return age
+
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        if not self.patient_id:
+            self.patient_id = "PAT-" + str(uuid.uuid4().hex[:8]).upper()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.patient_id} - {self.first_name} {self.last_name}"
     
+
+
 class Nurse(models.Model):
     nurse_id = models.CharField(max_length=20, unique=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- Backend/Hospital/core/serializers.py
+++ Backend/Hospital/core/serializers.py
@@ -1,6 +1,7 @@
+from datetime import date
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from .models import User, Doctor, Nurse, BillingStaff, Receptionist
+from .models import User, Doctor, Nurse, BillingStaff, Receptionist, Patient
 
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -73,3 +74,143 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data['role'] = self.user.role
         data['username'] = self.user.username
         return data
+
+
+class PatientRegistrationSerializer(serializers.ModelSerializer):
+    age = serializers.SerializerMethodField(method_name="calculate_age")
+
+    class Meta:
+        model = Patient
+        fields = [
+            "id",
+            "patient_id",
+            "first_name", 
+            "last_name", 
+            "dob", 
+            "gender", 
+            "phone", 
+            "email", 
+            "address", 
+            "emergency_contact", 
+            "insurance_provider", 
+            "policy_number", 
+            "age"
+        ]
+        read_only_fields = ["patient_id", "created_at","updated_at"]
+
+    def calculate_age(self, obj):
+        today = date.today()
+        age = today.year - obj.dob.year
+        if today < obj.dob.replace(year=today.year):
+            age -= 1
+        return age
+
+    def validate_dob(self, value):
+        today = date.today()
+        if value > today:
+            raise serializers.ValidationError("Date of birth cannot be in the future")
+
+        # Calculate precise age taking month/day into account
+        age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+
+        if age < 0 or age > 120:
+            raise serializers.ValidationError("Age must be between 0 and 120")
+        return value
+
+    def validate(self, data):
+        # Prevent duplicate registration
+        if Patient.objects.filter(phone=data["phone"]).exists():
+            raise serializers.ValidationError("Patient with this phone already exists")
+
+        if data.get("email"):
+            if Patient.objects.filter(email=data["email"]).exists():
+                raise serializers.ValidationError("Patient with this email already exists")
+
+        return data
+    
+class PatientUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Patient
+        fields = [
+            "first_name",
+            "last_name",
+            "dob",
+            "gender",
+            "phone",
+            "email",
+            "address",
+            "emergency_contact",
+            "insurance_provider",
+            "policy_number",
+        ]
+        extra_kwargs = {
+            field: {"required": False}
+            for field in fields
+        }
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+    def validate_dob(self, value):
+        today = date.today()
+        if value > today:
+            raise serializers.ValidationError("Date of birth cannot be in the future")
+
+        age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+
+        if age < 0 or age > 120:
+            raise serializers.ValidationError("Age must be between 0 and 120")
+        return value
+
+# serializers.py
+
+class PatientListSerializer(serializers.ModelSerializer):
+    age = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Patient
+        fields = [
+            "id",
+            "patient_id",
+            "first_name",
+            "last_name",
+            "phone",
+            "gender",
+            "age",
+            "created_at",
+            "updated_at"
+        ]
+
+
+# serializers.py
+
+class PatientDetailSerializer(serializers.ModelSerializer):
+
+    registered_by = serializers.StringRelatedField(source="user", read_only=True)
+    age = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Patient
+        fields = [
+            "id",
+            "patient_id",
+            "first_name",
+            "last_name",
+            "dob",
+            "gender",
+            "age",
+            "phone",
+            "email",
+            "address",
+            "emergency_contact",
+            "insurance_provider",
+            "policy_number",
+            "registered_by",
+            "created_at",
+            "updated_at",
+        ]
